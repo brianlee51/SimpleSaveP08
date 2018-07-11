cef2ad99ae2a04eb31c0a2da136dcdaa5dc47dd9
brianlee51 <17010546@myrp.edu.sg>
2018-07-11 10:15:20 +0800

    How to create a Shared Preferences

--- app/src/main/java/sg/edu/rp/c346/simplesave/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/simplesave/MainActivity.java
@@ -1,5 +1,8 @@
 package sg.edu.rp.c346.simplesave;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -15,10 +18,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        // Step 1a: Obtain an instance of the SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // Step 1b: Obatain an instance of the SharedPreference Editor for update later
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        // Step 1c: Add the key-value pair
+        prefEdit.putString("greetings","Hello!");
+        // Step 1d: Call commit() method to save the changes into the SharedPreferences
+        prefEdit.commit();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
 
-        String msg="No greetings!";
+//        String msg="No greetings!";
+
+        // Step 2a: Obtain an instance of the SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // Step 2b: Retrieve the saved data with the key "greetings" from the SharedPreferences object
+        String msg = prefs.getString("greetings", "No greetings!");
+
         Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG);
         toast.show();
     }
